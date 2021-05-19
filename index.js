5857bac271fd5a10cdf07018f73732139199f0b5
PMartinGranara <pmartingranara@gmail.com>
2021-05-19 21:18:53 +0200

    commit

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ app.get('/', (req, res) => {
 })
 
 app.get('/login.ejs', (req, res) => {
+  for (let i= 0; i<localStorage.length; i++) {
+    lstotoken = localStorage.getItem(localStorage.key(i))
+    lstotoken = JSON.parse(lstotoken)
+    if (lstotoken['token']) {
+      res.render('confirmacion', {"confirmacion": 2 });
+    }
+  }  
   res.render('login');
 })
 
@@ -25,7 +32,7 @@ app.get('/register.ejs', (req, res) => {
 app.post('/', (req, res) => {
   let user = req.body.user;
   let pass = req.body.password;
-  let data = {"password": pass, "token": ""};
+  let data = {"password": pass };
   localStorage.setItem(user,JSON.stringify(data));
   res.render('index');
 })
@@ -39,12 +46,27 @@ app.post('/confirmacion.ejs', (req, res) => {
   	if (user == localStorage.key(i)) {
   		let data = JSON.parse(localStorage.getItem(user));
   		if (pass == data['password']) {
+        updatedjson = localStorage.getItem(user);
+        updatedjson = JSON.parse(updatedjson);
+        let token = require("randomstring");
+        updatedjson['token'] = token.generate(10);
+        localStorage.setItem(user,JSON.stringify(updatedjson));
   			confirmacion = 1;
   		}
   	}
   }
 
   res.render('confirmacion', {"confirmacion": confirmacion });
+})
+
+app.get('/logout.ejs', (req, res) => {
+  for (let i= 0; i<localStorage.length; i++) {
+    lstotoken = localStorage.getItem(localStorage.key(i));
+    lstotoken = JSON.parse(lstotoken);
+    delete lstotoken['token'];
+    localStorage.setItem(localStorage.key(i), JSON.stringify(lstotoken));
+  }
+  res.render('index');
 })
 
 app.listen(4000, () => console.log('Example app listening on port 4000!'));
