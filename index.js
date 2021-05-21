98c0d95139b153dbaa3fec7b318b6b76c2e53e23
PMartinGranara <pmartingranara@gmail.com>
2021-05-21 20:10:04 +0200

    end finally

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.get('/logout.ejs', (req, res) => {
 
 
 
-app.post("/token", function(req, res){
+app.post("/api/login", function(req, res){
   
   user = req.headers['user'];
   password = req.headers['password'];
@@ -79,22 +79,55 @@ app.post("/token", function(req, res){
   for (let i= 0; i<localStorage.length; i++) {
   	if (user == localStorage.key(i)) {
   		let data = JSON.parse(localStorage.getItem(user));
-      res.send("Password "+data['password']);
-      res.send("Password "+password);
   		if (password == data['password']) {
         updatedjson = localStorage.getItem(user);
         updatedjson = JSON.parse(updatedjson);
         let token = require("randomstring");
         updatedjson['token'] = token.generate(10);
         localStorage.setItem(user,JSON.stringify(updatedjson));
-        return res.send(updatedjson);
+        return res.send( "Nou token generat ->  "+ updatedjson['token']+"\n");
   		}
-      return res.send("Contraseña incorrecta");
+      return res.send("Contraseña incorrecta\n");
   	}
   }
-  return res.send("Usuario no encontrado");
+  return res.send("Usuario no encontrado\n");
+})
+
+app.post("/api/logout", function (req, res){
 
 
+  user = req.headers['user'];
+  password = req.headers['password'];
+
+  for (let i= 0; i<localStorage.length; i++) {
+  	if (user == localStorage.key(i)) {
+  		let data = JSON.parse(localStorage.getItem(user));
+  		if (password == data['password']) {
+        delete data['token'];
+        localStorage.setItem(localStorage.key(i), JSON.stringify(data));
+        return res.send("Token del usuario "+user+" borrado\n");
+  		}
+      return res.send("Contraseña incorrecta\n");
+  	}
+  }
+  return res.send("Usuario no encontrado\n");
+
+
+
+
+
+
+
+
+
+
+  for (let i= 0; i<localStorage.length; i++) {
+    lstotoken = localStorage.getItem(localStorage.key(i));
+    lstotoken = JSON.parse(lstotoken);
+    delete lstotoken['token'];
+    localStorage.setItem(localStorage.key(i), JSON.stringify(lstotoken));
+  }
+  res.send("Tokens eliminados\n");
 })
 
 app.listen(4000, () => console.log('Example app listening on port 4000!'));
